f0b80b396625521a159ade2916b0572a21221276
hugosandsjo <hugosandsjo@gmail.com>
2024-07-29 17:25:27 +0200

    got inlog to work with Oauth

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,6 +5,12 @@ function Header() {
   return (
     <header className="h-28 px-10 flex justify-between items-center">
       <h1 className="text-4xl">Diary</h1>
+      <button className="border">
+        <a href="/api/auth/login">Login</a>
+      </button>
+      <button className="border">
+        <a href="/api/auth/logout">Logout</a>
+      </button>
       <Image src="/plus.svg" width={30} height={30} alt="plus icon" />
     </header>
   );
