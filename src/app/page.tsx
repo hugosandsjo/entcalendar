e91378a99797d6b6a5669cbaa5ac93552bcf9053
hugosandsjo <hugosandsjo@gmail.com>
2024-08-05 15:01:40 +0200

    try to move api endpoints in nested folders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
-// import dotenv from "dotenv";
-// dotenv.config({ path: ".env" }); // Ensure the path is correct if your .env file is named differently or located elsewhere
-
-console.log("POSTGRES_URL:", process.env.POSTGRES_URL);
-
 import EntryContainer from "./components/EntryContainer";
 import Main from "./components/Main";
 
