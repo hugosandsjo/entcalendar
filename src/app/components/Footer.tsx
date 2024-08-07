4e23300cb7530b4bb4c104b7a440242a5ecd8d08
hugosandsjo <hugosandsjo@gmail.com>
2024-08-07 13:13:42 +0200

    radio button now works but does not change the fields in the form

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import Link from "next/link";
 
 function Footer() {
   return (
     <header>
       <div className="flex justify-evenly p-12">
-        <p>Home</p>
-        <p>About</p>
-        <p>Contact</p>
+        <Link href="/">
+          {" "}
+          <p>Home</p>
+        </Link>
+        <Link href="/about">
+          <p>About</p>
+        </Link>
+        <Link href="/contact">
+          <p>Contact</p>
+        </Link>
       </div>
     </header>
   );
