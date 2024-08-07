f3db6bb54b5debe634f6e80a4081a117c2e2ba89
hugosandsjo <hugosandsjo@gmail.com>
2024-08-07 14:50:56 +0200

    didnt change form based on category but made the posts appear under the right heading

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,22 +1,24 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import LogButton from "./LogButton";
 
 function Header() {
   return (
     <header className="h-28 px-10 flex justify-between items-center">
       <Link href="/">
         {" "}
-        <h1 className="text-4xl">Diary</h1>
+        <h1 className="text-4xl hover:opacity-30">Diary</h1>
       </Link>
-      <button className="border">
-        <a href="/api/auth/login">Login</a>
-      </button>
-      <button className="border">
-        <a href="/api/auth/logout">Logout</a>
-      </button>
+      <LogButton></LogButton>
       <Link href="/new">
-        <Image src="/plus.svg" width={30} height={30} alt="plus icon" />
+        <Image
+          className="hover:opacity-30"
+          src="/plus.svg"
+          width={30}
+          height={30}
+          alt="plus icon"
+        />
       </Link>
     </header>
   );
